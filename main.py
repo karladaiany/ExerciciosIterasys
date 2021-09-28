61bd13caa05c8120411c2ae93dfb933a7f625cad
karladaiany <karllinhadeoliveira@live.com>
2021-09-27 22:41:00 -0300

    Atualização da Lista 06 com ajuste das funções e correção do teste unitário da tabuada.

--- main.py
+++ main.py
@@ -12,28 +12,34 @@ def calcular_fosforos(caixa):
     if caixa > 0:
         return caixa * 40
     else:
-        return 0
+        return 'Numero inválido. Digite um número positivo.'
 
 
 # 2 - Crie a função tabuada()
 # Na qual você informa um número e exibe a tabuada daquele numero:
 # Exemplo: 2, 4, 6, 8, etc
 
-def tabuada(numero,vezes):
-    for qtde in range(vezes):
-        resultado = numero * qtde
-        print(f'{numero}x{qtde}={resultado}')
+def tabuada(numero):
+    lista_resultado = []
+    for count in range(11):
+        resultado = int((numero)) * count
+        print(f'{numero}x{count}={resultado}')
+        lista_resultado.insert(count, resultado)
+    return lista_resultado
 
 
 if __name__ == '__main__':
     print_hi('KarlaDaiany')
 
-# calcular palitos de fósforo
-resultado = calcular_fosforos(3)
-print(f'A quantidade de palitos de fósforos é de: {resultado}')
+    # calcular palitos de fósforo
+    caixa = input(f'Digite a quantidade de caixa de fósforo: ')
+    resultado = calcular_fosforos(int(caixa))
+    print(f'A quantidade de palitos de fósforos é de: {resultado}')
 
-# calcular a tabuada
-tabuada(3,11)
+    # calcular a tabuada
+    numero = input(f'Digite o número para gerar a tabuada: ')
+    print(f'### TABUADA DO {numero} ###')
+    tabuada(numero)
 
 
 # 3 - Crie pelo menos um teste unitário para cada uma das funções anteriores
@@ -46,8 +52,17 @@ def test_calcular_fosforos_decimal():
     assert calcular_fosforos(1.5) == 60
 
 def test_calcular_fosforos_negativo():
-    assert calcular_fosforos(-1) == 0
+    assert calcular_fosforos(-1) == 'Numero inválido. Digite um número positivo.'
 
 # Teste da função tabuada
-def test_tabuada():                         #Teste BUGADO! FALSO POSITIVO! VALIDAR!!!
-    assert tabuada(2,3) == print(0,2,4)
+def test_tabuada():
+
+    #Configura
+    numero = 3
+    resultado_esperado = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
+
+    #Executa
+    resultado_atual = tabuada(3)
+
+    #Check
+    assert resultado_atual == resultado_esperado
